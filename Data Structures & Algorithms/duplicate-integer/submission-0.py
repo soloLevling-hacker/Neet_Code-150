3b315a93de1b13c966249b53e68a0d2b50d4cd9b
soloLevling-hacker <khushrupapara5@gmail.com>
2026-07-14 10:02:58 +0530

    Refactor duplicate detection logic in hasDuplicate
    Refactor hasDuplicate method to improve readability.

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,10 +1,19 @@
+'''
+Approach:
+1.	Use a hash set (set) to store the numbers seen so far.
+2.	Traverse the array once.
+3.	If the current number is already in the set, a duplicate exists, so return True.
+4.	Otherwise, add the number to the set.
+5.	If no duplicate is found after traversing the entire array, return False.
+'''
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        
         seen = set()
-
         for num in nums:
             if num in seen:
                 return True
             seen.add(num)
         return False
+    
+
+
